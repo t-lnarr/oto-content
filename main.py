e3d2ed9c62b4bba0f1850e8c39732e23453887e2
Yusup <telnarr.dev@gmail.com>
2025-10-15 22:50:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -144,6 +144,7 @@ async def main():
     scheduler.add_job(daily_post, "cron", hour=15, minute=0, timezone=TIMEZONE)
     scheduler.add_job(daily_post, "cron", hour=18, minute=0, timezone=TIMEZONE)
     scheduler.add_job(daily_post, "cron", hour=21, minute=0, timezone=TIMEZONE)
+    scheduler.add_job(daily_post, "cron", hour=23, minute=0, timezone=TIMEZONE)
 
     scheduler.start()
     print("Bot çalışıyor... ⏰")
